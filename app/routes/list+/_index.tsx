a990b07e85b634f29ab67804b400d6061f6d190b
collins401 <373345619@qq.com>
2024-04-19 21:02:24 +0800

    feat: add model page

--- app/routes/list+/_index.tsx
+++ app/routes/list+/_index.tsx
@@ -1,50 +1,7 @@
-import { Link } from '@remix-run/react'
-import { useAntdTable } from 'ahooks'
-import { Form, Segmented, theme } from 'antd'
-import { getUserList } from '../system+/api'
-
-import code1 from '~/assets/images/code1.png'
-import SearchForm, { SearchOption } from '~/components/searchForm'
-
-const getTableData = ({ current, pageSize }: any, formData: any): Promise<any> => {
-  const params = {
-    pageSize,
-    pageNum: current,
-    ...formData
-  }
-  console.log('111')
-  return getUserList(params).then(res => ({
-    total: res.total,
-    list: res.rows
-  }))
-}
-
 export default function List() {
-  const [form] = Form.useForm()
-  const { token } = theme.useToken()
-  const { tableProps, loading, search } = useAntdTable(getTableData)
-  const options: SearchOption[] = [
-    { label: '订单号', name: 'orderNo', itemType: 'input' },
-    { label: '业主电话', name: 'ownerPhone', itemType: 'select', options: [] },
-    { label: '业主姓名', name: 'ownerName', itemType: 'input' },
-    { label: '业主姓名1', name: 'ownerName2', itemType: 'rangePicker' }
-  ]
-  const tabs = ['常规搜索', '联动搜索', '展开/折叠', '动态数据']
   return (
-    <div className="p-3 rounded" style={{ background: token.colorBgContainer }}>
-      <Segmented<string>
-        options={tabs}
-        onChange={value => {
-          console.log(value) // string
-        }}
-      />
-      <SearchForm form={form} options={options} onChange={search.submit} variant="outlined" />
-      <h3>代码实现：</h3>
-      <div style={{ background: token.colorFillAlter }} className="">
-        <img src={code1} alt="code1" />
-      </div>
-      <Link to="/list/detail">to detail</Link>
-      <div className=" p-10">12</div>
+    <div className="p-3 rounded">
+      <div>index page</div>
     </div>
   )
 }
---
+++ app/routes/list+/model.tsx
@@ -0,0 +1,27 @@
+import { Form, redirect, useNavigate } from '@remix-run/react'
+import { Modal } from 'antd'
+
+export async function cliendLoader() {
+  // your code
+  return redirect('/list/model')
+}
+
+export default function MemberDisableRoute() {
+  const navigate = useNavigate()
+  return (
+    <Modal open={true} onCancel={() => navigate('/list')}>
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Are you sure?</h3>
+        <div className="modal-action">
+          <button className="btn" onClick={() => navigate(-1)}>
+            Cancel
+          </button>
+          <Form method="POST">
+            <input name="id" value="id" hidden />
+            <button className="btn">Yes</button>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  )
+}
